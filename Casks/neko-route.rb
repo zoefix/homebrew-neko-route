6141c634834b57fe5ac33400e2b6cc8ffda80dfc
zoefix <277453498+zoefix@users.noreply.github.com>
2026-06-26 03:49:25 +0000

    Update neko-route cask to 0.2.1

--- Casks/neko-route.rb
+++ Casks/neko-route.rb
@@ -1,9 +1,9 @@
 cask "neko-route" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.2.0"
-  sha256 arm:   "8cae4e02b571f1859d6e32587dbe440905f945d37f7ff50d8d7cfeeff33a260c",
-         intel: "0abcf1e73164a5068a3465884d9f263c1de148250f83337b739b40dfe633056a"
+  version "0.2.1"
+  sha256 arm:   "8568d9ff2139c4ad736d705f89711b69ffd330346e71ee6a79bb1b10eabf9192",
+         intel: "de76903965fc363621b077011df309d0f02a0e46fcb82ef3d8a8abb0f9e0dc18"
 
   url "https://github.com/zoefix/neko-route/releases/download/v#{version}/Neko-Route_#{version}_macos_#{arch}.dmg"
   name "Neko Route"
